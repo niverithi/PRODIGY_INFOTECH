5cacf478483c166f64ddf05bcea63c819b280e4a
NivethaSB <119474007+niverithi@users.noreply.github.com>
2024-07-18 20:43:44 +0530

    Add files via upload

---
+++ Prodigy_FS_02/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Simple Login Form Example</h1>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ Prodigy_FS_02/LoginForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = event => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // You can add logic here to handle form submission, such as calling an API for authentication
+  };
+
+  return (
+    <div>
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
